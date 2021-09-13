c806866f6b782769f113194187e16e1071d5e6f1
Tina Pham <tina.pham8912@gmail.com>
2021-09-13 11:58:39 -0700

    tech blog is working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const blogRoutes = require('./Routes');
+const blogRoutes = require('./blogRoutes');
 
 router.use('/users', userRoutes);
 router.use('/blogs', blogRoutes);
